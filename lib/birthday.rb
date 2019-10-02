3cdda637b63cc4b3d9e5b98ab517b935df3b15ee
cedric e goddard <8ceg6@comcast.net>
2019-10-02 21:06:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,8 @@ def happy_birthday(birthday_kids)
   end
 end
 
+age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |kids_name,age|
+          if age <       
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 
